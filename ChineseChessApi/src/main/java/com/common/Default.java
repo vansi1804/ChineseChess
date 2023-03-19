7be23e9b9a0a306191b596fdac51a373efa03781
Nguyen Van Si <vansi18042001@gmail.com>
2023-03-19 15:49:39 +0700

    refactor source

---
+++ ChineseChessApi/src/main/java/com/common/Default.java
@@ -0,0 +1,7 @@
+package com.common;
+
+public class Default {
+    public static int ELO_SCORE = 2000;
+    public static int ROLE_ID = 2; // id = 1 for admin
+    public static int LEVELS_ID = 1;
+}
