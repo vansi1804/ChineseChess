dcceee0356be070c84bad8a7e3947e7794288bc8
Nguyen Van Si <vansi18042001@gmail.com>
2023-07-02 13:46:47 +0700

    add desktop app project

--- ChineseChessApi/src/main/java/com/common/ErrorMessage.java
+++ ChineseChessApi/src/main/java/com/common/ErrorMessage.java
@@ -1,6 +1,7 @@
 package com.common;
 
 public class ErrorMessage {
+
     public static final String INVALID_DATA = "Invalid data";
     public static final String INVALID_MOVING_PLAYER = "Invalid moving player";
     public static final String INVALID_MOVE = "Invalid move";
@@ -13,7 +14,8 @@ public class ErrorMessage {
     public static final String DATA_NOT_FOUND = "Data not found";
     public static final String DATA_ALREADY_EXISTS = "Data already exists";
     public static final String BLANK_DATA = "Blank data";
-    public static final String PASSWORD_TOO_SHORT = "Password should have at least"
+    public static final String INVALID_PASSWORD_SIZE = "Password should have at least"
             + Validation.PASSWORD_SIZE_MIN + " characters";
     public static final String INVALID_FILE_TYPE = "Invalid file type";
+
 }
