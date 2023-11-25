dbf8e60d29b4d9bcc81669c72f917439c6800cc5
vansi1804 <vansi18042001@gmail.com>
2023-11-25 11:12:09 +0700

    merge constant into ApiUrl

--- ChineseChessApi/src/main/java/com/common/ApiUrl.java
+++ ChineseChessApi/src/main/java/com/common/ApiUrl.java
@@ -2,26 +2,30 @@ package com.common;
 
 public class ApiUrl {
 
-    public static final String RANKS = Constant.API + Constant.ADMIN + Constant.RANK;
+    public static final String API = "/api";
+
+    public static final String ADMIN = "/admin";
+
+    public static final String RANKS = API + ADMIN + "/ranks";
     
-    public static final String ROLES = Constant.API + Constant.ADMIN + Constant.ROLE;
+    public static final String ROLES = API + ADMIN + "/roles";
     
-    public static final String VIPS = Constant.API + Constant.ADMIN + Constant.VIP;
+    public static final String VIPS = API + ADMIN + "/vips";
     
-    public static final String FILES = Constant.API + Constant.FILE;
+    public static final String FILES = API + "/files";
     
-    public static final String MATCHES = Constant.API + Constant.MATCH;
+    public static final String MATCHES = API + "/matches";
     
-    public static final String MOVES = Constant.API + Constant.MOVE;
+    public static final String MOVES = API + "/moves";
     
-    public static final String PLAYERS = Constant.API + Constant.PLAYER;
+    public static final String PLAYERS = API + "/players";
     
-    public static final String TRAININGS = Constant.API + Constant.TRAINING;
+    public static final String TRAININGS = API + "/trainings";
     
-    public static final String AUTH = Constant.API + Constant.AUTH;
+    public static final String AUTH = API + "/auth";
     
-    public static final String USER = Constant.API + Constant.USER;
+    public static final String USER = API + "/users";
     
-    public static final String PLAY_BOARD = Constant.API + Constant.PLAY_BOARD;
+    public static final String PLAY_BOARD = API + "/play-board";
 
 }
