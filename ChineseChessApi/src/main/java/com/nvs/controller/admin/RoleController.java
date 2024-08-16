8b9ed3d17adf92880479f2df08e53d2681349b03
vansi1804 <vansi18042001@gmail.com>
2024-08-16 09:15:01 +0700

    add api docs

--- ChineseChessApi/src/main/java/com/nvs/controller/admin/RoleController.java
+++ ChineseChessApi/src/main/java/com/nvs/controller/admin/RoleController.java
@@ -3,6 +3,8 @@ package com.nvs.controller.admin;
 import com.nvs.common.ApiUrl;
 import com.nvs.service.RoleService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(ApiUrl.ROLES)
 @RequiredArgsConstructor
-@Tag(name = "Role", description = "Endpoints for managing Roles - requires 'ADMIN' role")
-class RoleController{
+@Tag(name = "Role", description = "Endpoints for managing roles - requires 'ADMIN' role")
+public class RoleController {
 
-   private final RoleService roleService;
+  private final RoleService roleService;
 
-   @Operation(summary = "Get all roles", description = "Endpoint to retrieve all roles")
-   @PreAuthorize(value = "hasAuthority('ADMIN')")
-   @GetMapping(value = "")
-   public ResponseEntity<?> findAll(){
-      return ResponseEntity.ok(roleService.findAll());
-   }
-
+  @Operation(summary = "Get all roles", description = "Retrieve a list of all roles.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successfully retrieved all roles"),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
+  @PreAuthorize(value = "hasAuthority('ADMIN')")
+  @GetMapping(value = "")
+  public ResponseEntity<?> findAll() {
+    return ResponseEntity.ok(roleService.findAll());
+  }
 }
